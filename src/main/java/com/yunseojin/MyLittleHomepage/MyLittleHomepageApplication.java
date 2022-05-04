5a8dd608e8f48ab3c197502ccd9a963f0a36e14c
YunSeojin <tkaltk123@gmail.com>
2022-05-05 05:38:54 +0900

    init

---
+++ src/main/java/com/yunseojin/MyLittleHomepage/MyLittleHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.yunseojin.MyLittleHomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLittleHomepageApplication.class, args);
+	}
+
+}
