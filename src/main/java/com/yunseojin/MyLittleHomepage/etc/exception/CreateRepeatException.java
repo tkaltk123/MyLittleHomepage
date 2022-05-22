cfd2fb7b7d59e3cdb9733658495947b862cff7de
YunSeojin <tkaltk123@gmail.com>
2022-05-23 06:25:44 +0900

    게시글/댓글 중복 예외에 남은시간 추가

--- src/main/java/com/yunseojin/MyLittleHomepage/etc/exception/CreateRepeatException.java
+++ src/main/java/com/yunseojin/MyLittleHomepage/etc/exception/CreateRepeatException.java
@@ -1,8 +1,16 @@
 package com.yunseojin.MyLittleHomepage.etc.exception;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.yunseojin.MyLittleHomepage.etc.enums.ErrorMessage;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({"cause", "stackTrace", "message", "localizedMessage", "message", "suppressed"})
 public class CreateRepeatException extends BaseException {
     protected int waitSeconds;
 
