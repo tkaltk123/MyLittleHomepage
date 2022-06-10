7f650016a6934ad121a7536d7ce1f60871c5377d
YunSeojin <tkaltk123@gmail.com>
2022-06-10 23:56:21 +0900

    기본 logout 제거, 바디헤더, 풋터 분리

--- src/main/java/com/yunseojin/MyLittleHomepage/etc/config/SecurityConfig.java
+++ src/main/java/com/yunseojin/MyLittleHomepage/etc/config/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable().csrf().disable();
+        security
+                .httpBasic().disable()
+                .csrf().disable()
+                .logout().disable();
     }
 }
