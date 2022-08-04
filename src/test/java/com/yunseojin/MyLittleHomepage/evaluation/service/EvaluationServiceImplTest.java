89eaf30d2ef59427e42a4603bfbccca2abf0b70b
YunSeojin <tkaltk123@gmail.com>
2022-08-04 13:54:46 +0900

    평가 테스트 수정

--- src/test/java/com/yunseojin/MyLittleHomepage/evaluation/service/EvaluationServiceImplTest.java
+++ src/test/java/com/yunseojin/MyLittleHomepage/evaluation/service/EvaluationServiceImplTest.java
@@ -1,11 +1,12 @@
 package com.yunseojin.MyLittleHomepage.evaluation.service;
 
+import com.yunseojin.MyLittleHomepage.board.entity.BoardEntity;
+import com.yunseojin.MyLittleHomepage.board.repository.BoardRepository;
 import com.yunseojin.MyLittleHomepage.comment.dto.CommentRequest;
 import com.yunseojin.MyLittleHomepage.comment.entity.CommentEntity;
 import com.yunseojin.MyLittleHomepage.comment.repository.CommentRepository;
 import com.yunseojin.MyLittleHomepage.comment.service.CommentServiceImpl;
 import com.yunseojin.MyLittleHomepage.etc.enums.ErrorMessage;
-import com.yunseojin.MyLittleHomepage.etc.exception.BadRequestException;
 import com.yunseojin.MyLittleHomepage.member.dto.MemberRequest;
 import com.yunseojin.MyLittleHomepage.member.service.MemberServiceImpl;
 import com.yunseojin.MyLittleHomepage.post.dto.PostRequest;
@@ -20,11 +21,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.yunseojin.MyLittleHomepage.TestUtil.assertError;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
 class EvaluationServiceImplTest {
+
     @Autowired
     private MemberServiceImpl memberService;
     @Autowired
@@ -33,112 +36,150 @@ class EvaluationServiceImplTest {
     private CommentServiceImpl commentService;
     @Autowired
     private EvaluationServiceImpl evaluationService;
+
+    @Autowired
+    private BoardRepository boardRepository;
     @Autowired
     private PostRepository postRepository;
     @Autowired
     private CommentRepository commentRepository;
 
-    private static MemberRequest loginReq;
-    private static PostRequest postReq;
-    private static final Long testBoardId = 1L;
-
-    @BeforeAll
-    public static void setUp() {
-        loginReq = MemberRequest.builder()
-                .loginId("testuser")
-                .password("1234")
-                .nickname("testuser")
-                .build();
-        postReq = PostRequest.builder()
-                .title("제목")
-                .content("내용")
-                .hashTags(new String[]{"태그1", "태그2"})
-                .build();
-    }
+    private static MemberRequest memberRequest;
+    private static PostRequest postRequest;
+    private static CommentRequest commentRequest;
 
     private PostEntity post;
     private CommentEntity comment;
 
-    @BeforeEach
-    public void init() {
-        var commentReq = CommentRequest.builder()
+    @BeforeAll
+    public static void setUp() {
+
+        memberRequest = MemberRequest.builder()
+                .loginId("testuser")
+                .password("1234")
+                .nickname("testuser")
+                .build();
+
+        postRequest = PostRequest.builder()
+                .title("제목")
+                .content("내용")
+                .hashTags(new String[]{"태그1", "태그2"})
+                .build();
+
+        commentRequest = CommentRequest.builder()
                 .content("댓글")
                 .build();
-        memberService.register(loginReq);
-        var postRes = postService.createPost(testBoardId, postReq);
-        post = postRepository.getPost(postRes.getId());
-        var commentRes = commentService.createComment(postRes.getId(), commentReq);
-        comment = commentRepository.getComment(commentRes.getId());
+    }
+
+    @BeforeEach
+    public void init() {
+
+        var board = BoardEntity.builder().
+                name("testBoard")
+                .build();
+
+        memberService.register(memberRequest);
+
+        board = boardRepository.save(board);
+        var postRes = postService.createPost(board.getId(), postRequest);
+        var commentRes = commentService.createComment(postRes.getId(), commentRequest);
+
+        post = postRepository.findById(postRes.getId()).get();
+        comment = commentRepository.findById(commentRes.getId()).get();
         memberService.logout();
     }
 
     @Test
     void post() {
-        var postId = post.getId();
+        
         //로그인 안됨
-        assertEquals(ErrorMessage.NOT_LOGIN_EXCEPTION.getCode(),
-                assertThrows(BadRequestException.class,
-                        () -> evaluationService.likePost(post.getId())
-                ).getCode());
-        memberService.login(loginReq);
+        assertError(ErrorMessage.NOT_LOGIN_EXCEPTION, () ->
+                evaluationService.likePost(post.getId())
+        );
+        assertError(ErrorMessage.NOT_LOGIN_EXCEPTION, () ->
+                evaluationService.dislikePost(post.getId())
+        );
+        
+        //로그인
+        memberService.login(memberRequest);
+        
         //게시글 없음
-        assertEquals(ErrorMessage.NOT_EXISTS_POST_EXCEPTION.getCode(),
-                assertThrows(BadRequestException.class,
-                        () -> evaluationService.likePost(0L)
-                ).getCode());
-        var likeCount = post.getPostCount().getLikeCount();
-        var dislikeCount = post.getPostCount().getDislikeCount();
-        assertEquals("좋아요 성공", evaluationService.likePost(postId));
-        assertEquals(post.getPostCount().getLikeCount(), likeCount + 1);
-        assertEquals("좋아요 취소", evaluationService.likePost(postId));
-        assertEquals(post.getPostCount().getLikeCount(), likeCount);
-        assertEquals("싫어요 성공", evaluationService.dislikePost(postId));
-        assertEquals(post.getPostCount().getDislikeCount(), dislikeCount + 1);
-        assertEquals("싫어요 취소", evaluationService.dislikePost(postId));
-        assertEquals(post.getPostCount().getDislikeCount(), dislikeCount);
-        //싫어요 후 좋아요
-        evaluationService.dislikePost(postId);
-        evaluationService.likePost(postId);
-        assertEquals(post.getPostCount().getLikeCount(), likeCount + 1);
-        assertEquals(post.getPostCount().getDislikeCount(), dislikeCount);
+        assertError(ErrorMessage.NOT_EXISTS_POST_EXCEPTION, () ->
+                evaluationService.likePost(0L)
+        );
+        assertError(ErrorMessage.NOT_EXISTS_POST_EXCEPTION, () ->
+                evaluationService.dislikePost(0L)
+        );
+        
+        //게시글 좋아요
+        evaluationService.likePost(post.getId());
+        assertEquals(1,post.getLikeCount());
+        
+        //좋아요 취소
+        evaluationService.likePost(post.getId());
+        assertEquals(0,post.getLikeCount());
+        
         //좋아요 후 싫어요
-        evaluationService.dislikePost(postId);
-        assertEquals(post.getPostCount().getLikeCount(), likeCount);
-        assertEquals(post.getPostCount().getDislikeCount(), dislikeCount + 1);
+        evaluationService.likePost(post.getId());
+        evaluationService.dislikePost(post.getId());
+        assertEquals(0,post.getLikeCount());
+        assertEquals(1,post.getDislikeCount());
+        
+        //싫어요 취소
+        evaluationService.dislikePost(post.getId());
+        assertEquals(0,post.getDislikeCount());
+        
+        //싫어요 후 좋아요
+        evaluationService.dislikePost(post.getId());
+        evaluationService.likePost(post.getId());
+        assertEquals(0,post.getDislikeCount());
+        assertEquals(1,post.getLikeCount());
     }
 
     @Test
     void comment() {
-        var commentId = comment.getId();
+        
         //로그인 안됨
-        assertEquals(ErrorMessage.NOT_LOGIN_EXCEPTION.getCode(),
-                assertThrows(BadRequestException.class,
-                        () -> evaluationService.likeComment(comment.getId())
-                ).getCode());
-        memberService.login(loginReq);
-        //게시글 없음
-        assertEquals(ErrorMessage.NOT_EXISTS_COMMENT_EXCEPTION.getCode(),
-                assertThrows(BadRequestException.class,
-                        () -> evaluationService.likeComment(0L)
-                ).getCode());
-        var likeCount = comment.getCommentCount().getLikeCount();
-        var dislikeCount = comment.getCommentCount().getDislikeCount();
-        assertEquals("좋아요 성공", evaluationService.likeComment(commentId));
-        assertEquals(comment.getCommentCount().getLikeCount(), likeCount + 1);
-        assertEquals("좋아요 취소", evaluationService.likeComment(commentId));
-        assertEquals(comment.getCommentCount().getLikeCount(), likeCount);
-        assertEquals("싫어요 성공", evaluationService.dislikeComment(commentId));
-        assertEquals(comment.getCommentCount().getDislikeCount(), dislikeCount + 1);
-        assertEquals("싫어요 취소", evaluationService.dislikeComment(commentId));
-        assertEquals(comment.getCommentCount().getDislikeCount(), dislikeCount);
-        //싫어요 후 좋아요
-        evaluationService.dislikeComment(commentId);
-        evaluationService.likeComment(commentId);
-        assertEquals(comment.getCommentCount().getLikeCount(), likeCount + 1);
-        assertEquals(comment.getCommentCount().getDislikeCount(), dislikeCount);
+        assertError(ErrorMessage.NOT_LOGIN_EXCEPTION, () ->
+                evaluationService.likeComment(comment.getId())
+        );
+        assertError(ErrorMessage.NOT_LOGIN_EXCEPTION, () ->
+                evaluationService.dislikeComment(comment.getId())
+        );
+
+        //로그인
+        memberService.login(memberRequest);
+
+        //댓글 없음
+        assertError(ErrorMessage.NOT_EXISTS_COMMENT_EXCEPTION, () ->
+                evaluationService.likeComment(0L)
+        );
+        assertError(ErrorMessage.NOT_EXISTS_COMMENT_EXCEPTION, () ->
+                evaluationService.likeComment(0L)
+        );
+
+        //댓글 좋아요
+        evaluationService.likeComment(comment.getId());
+        assertEquals(1,comment.getLikeCount());
+
+        //좋아요 취소
+        evaluationService.likeComment(comment.getId());
+        assertEquals(0,comment.getLikeCount());
+
         //좋아요 후 싫어요
-        evaluationService.dislikeComment(commentId);
-        assertEquals(comment.getCommentCount().getLikeCount(), likeCount);
-        assertEquals(comment.getCommentCount().getDislikeCount(), dislikeCount + 1);
+        evaluationService.likeComment(comment.getId());
+        evaluationService.dislikeComment(comment.getId());
+        assertEquals(0,comment.getLikeCount());
+        assertEquals(1,comment.getDislikeCount());
+
+        //싫어요 취소
+        evaluationService.dislikeComment(comment.getId());
+        assertEquals(0,comment.getDislikeCount());
+
+        //싫어요 후 좋아요
+        evaluationService.dislikeComment(comment.getId());
+        evaluationService.likeComment(comment.getId());
+        assertEquals(0,comment.getDislikeCount());
+        assertEquals(1,comment.getLikeCount());
     }
 }
